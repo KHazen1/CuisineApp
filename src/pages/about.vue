4fc5b0e9f9de7486bbac82c57806aa8ebcfe91c0
Kris <you@example.com>
2022-04-02 21:17:50 -0500

    Added about page description/Api framework

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -7,8 +7,17 @@
         <h2
          class="py-4 text-6xl tracking-tighter font-extralight"
         >
-         About Page
+         The Story of Compact Cuisine
         </h2>
+        <p class="mt-6 leading-relaxed">
+            Good food and good times, food is our love that is the message we 
+            strive to follow with Compact Cuisine. This App was created with this message
+            in mind throughout the entire process. An application that can help bring new fresh
+            ideas to your meal time conveniently and help with the cooking process 
+            along the way. Keep all your shopping and recipes needs in one easy to
+            use place! 
+        </p>
         <img src="../assets/bgabout.png" alt="Hello BG" />
     </div>
+
 </template>
--- src/pages/recipes.vue
+++ src/pages/recipes.vue
@@ -1,5 +1,15 @@
 <template>
-  <p>
-    Recipe Storage
-  </p>
+  <div class="container">  
+    <div class="text-center">
+      <h3 class="text-5xl">
+        Meal Time!
+      </h3>
+      <h5 class="text-2xl">
+        Click here for random recipe
+      </h5>
+      <button class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700" id="get_recipe">
+        Get Recipe 🍔
+      </button>
+    </div>
+  </div>
 </template>
